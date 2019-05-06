b848eaf14dfd85b0d568f5a7e972de1cf4c9db4f
Chris K.Y. FUNG <chris.ky.fung@connect.polyu.hk>
2019-05-06 22:45:01 +0800

    from khris

---
+++ diamond lattice/batch_sim.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+# ../../lmp_ubuntu_atomistica < in.2C.lmp -v s tersoff -v p SiC.tersoff
+# ../../lmp_ubuntu_atomistica < in.2C.lmp -v s tersoff -v p SiC_1989.tersoff
+# ../../lmp_ubuntu_atomistica < in.2C.lmp -v s tersoff -v p SiC_1990.tersoff
+# ../../lmp_ubuntu_atomistica < in.2C.lmp -v s tersoff -v p SiC_1994.tersoff
+# ../../lmp_ubuntu_atomistica < in.2C.lmp -v s tersoff -v p SiCGe.tersoff
+../../lmp_ubuntu_atomistica < in.3cSiC.lmp -v s tersoff -v p SiC_Erhart-Albe.tersoff
+# ../../lmp_ubuntu_atomistica < in.2C.lmp -v s tersoff -v p BNC.tersoff
+ #../../lmp_ubuntu_atomistica < in.2C.lmp -v s atomistica -v p Tersoff
+# sleep 5
+# ../../lmp_ubuntu_atomistica < in.2C.lmp -v s atomistica -v p TersoffScr
+# sleep 5
+# ../../lmp_ubuntu_atomistica < in.2C.lmp -v s atomistica -v p Brenner
+# sleep 5
+# ../../lmp_ubuntu_atomistica < in.2C.lmp -v s atomisticax -v P Brenner -v p Henriksson_PRB_79_144107_FeC
+# ../../lmp_ubuntu_atomistica < in.2C.lmp -v s atomisticax -v P Brenner -v p Albe_PRB_65_195124_PtC
+#../../lmp_ubuntu_atomistica < in.2C.lmp -v s atomistica -v p BrennerScr
+# sleep 5
+# ../../lmp_ubuntu_atomistica < in.2C.lmp -v s atomistica -v p Rebo2
+# sleep 5
+# ../../lmp_ubuntu_atomistica < in.2C.lmp -v s atomistica -v p Rebo2Scr
+# sleep 5
+# ../../lmp_ubuntu_atomistica < in.2C.lmp -v s atomistica -v p Juslin
+# sleep 5
+# ../../lmp_ubuntu_atomistica < in.2C.lmp -v s meam -v p SiC.meam
+# ../../lmp_ubuntu_atomistica < in.2C.lmp -v s airebo -v p CH.airebo
+# ../../lmp_ubuntu_atomistica < in.2C.lmp -v s rebo -v p CH.airebo
+# ../../lmp_ubuntu_atomistica < in.2C.lmp -v s lcbop -v p C.lcbop
+# ../../lmp_ubuntu_atomistica < in.2C.lmp -v s zbl -v p SiC.tersoff.zbl
+# ../../lmp_ubuntu_atomistica < in.2C.lmp -v s zbl -v p FeC_Henriksson_2013.tersoff.zbl
+# ../../lmp_ubuntu_atomistica < in.2C.lmp -v s comb3 -v p ffield.comb3
+
